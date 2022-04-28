a2ea075d87ab067558e8fb4c07cf85f721230e1e
Mahmut Can Özgan <ozgancan9@gmail.com>
2022-04-29 02:17:33 +0300

    Finish

--- src/components/Favorie.jsx
+++ src/components/Favorie.jsx
@@ -1,10 +1,23 @@
-
 import Title from "components/ui/Title";
+import Products from "api/product.json";
+import { useEffect, useState } from "react";
+import Product from "./ui/Product";
 
 export default function Favorie() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts(Products);
+  }, []);
   return (
     <div>
       <Title>Favoriler</Title>
+      <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+        {products.length &&
+          products.map((product, index) => (
+            <Product key={index} product={product} />
+          ))}
+      </div>
     </div>
-  )
+  );
 }
